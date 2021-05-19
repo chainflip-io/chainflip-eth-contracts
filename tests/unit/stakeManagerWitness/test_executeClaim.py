5816856633045b1973589b0db02cb34f23a096e8
Kyle Zsembery <zsembery.kyle@gmail.com>
2021-05-19 13:43:44 +1000

    add stakeManagerWitness tests which are a subset of the unit tests

---
+++ tests/unit/stakeManagerWitness/test_executeClaim.py
@@ -0,0 +1,54 @@
+from consts import *
+from shared_tests import *
+from brownie import reverts, web3, chain
+from brownie.test import given, strategy
+
+
+# Need to also register a claim in this since the amounts sent etc depend on registerClaim
+@given(
+    nodeID=strategy('uint', exclude=0),
+    amount=strategy('uint', max_value=MIN_STAKE*2, exclude=0),
+    staker=strategy('address'),
+    expiryTimeDiff=strategy('uint', min_value=CLAIM_DELAY, max_value=2*CLAIM_DELAY),
+    sleepTime=strategy('uint', min_value=5, max_value=3*CLAIM_DELAY)
+)
+def test_executeClaim_rand(cf, stakedMin, nodeID, amount, staker, expiryTimeDiff, sleepTime):
+    assert cf.stakeManager.getPendingClaim(nodeID) == NULL_CLAIM
+    smStartBal = cf.flip.balanceOf(cf.stakeManager)
+    stakerStartBal = cf.flip.balanceOf(staker)
+
+    expiryTime = chain.time() + expiryTimeDiff + 5
+    args = (nodeID, amount, staker, expiryTime)
+    callDataNoSig = cf.stakeManager.registerClaim.encode_input(NULL_SIG_DATA, *args)
+    tx1 = cf.stakeManager.registerClaim(AGG_SIGNER_1.getSigData(callDataNoSig), *args)
+    
+    assert cf.stakeManager.getPendingClaim(nodeID) == (amount, staker, tx1.timestamp + CLAIM_DELAY, expiryTime)
+    assert cf.flip.balanceOf(cf.stakeManager) == smStartBal
+
+    # Want to calculate inflation 1 block into the future because that's when the tx will execute
+    newLastMintBlockNum = web3.eth.blockNumber + 1
+    inflation = getInflation(cf.stakeManager.tx.block_number, newLastMintBlockNum, EMISSION_PER_BLOCK)
+    maxValidAmount = cf.flip.balanceOf(cf.stakeManager) + inflation
+
+    chain.sleep(sleepTime)
+    
+    if chain.time() < tx1.timestamp + CLAIM_DELAY or chain.time() > expiryTime:
+        with reverts(REV_MSG_NOT_ON_TIME):
+            cf.stakeManager.executeClaim(nodeID)
+    elif amount > maxValidAmount:
+        with reverts(REV_MSG_EXCEED_BAL):
+            cf.stakeManager.executeClaim(nodeID)
+    else:
+        tx = cf.stakeManager.executeClaim(nodeID)
+        
+        # Check things that should've changed
+        assert cf.stakeManager.getPendingClaim(nodeID) == NULL_CLAIM
+        assert newLastMintBlockNum == tx.block_number
+        assert cf.stakeManager.getLastMintBlockNum() == newLastMintBlockNum
+        assert cf.flip.balanceOf(cf.stakeManager) == maxValidAmount - amount
+        assert cf.stakeManager.getTotalStakeInFuture(0) == maxValidAmount - amount
+        assert tx.events["ClaimExecuted"][0].values() == [nodeID, amount]
+        assert cf.flip.balanceOf(staker) == stakerStartBal + amount
+        # Check things that shouldn't have changed
+        assert cf.stakeManager.getEmissionPerBlock() == EMISSION_PER_BLOCK
+        assert cf.stakeManager.getMinimumStake() == MIN_STAKE
