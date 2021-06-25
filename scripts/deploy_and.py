7b3a1f992f319acd1de818d86aedfb412ed5d441
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2021-06-25 12:28:30 +0200

    Fixing deplyo_and script

--- scripts/deploy_and.py
+++ scripts/deploy_and.py
@@ -2,9 +2,10 @@ import sys
 from os import path
 sys.path.append(path.abspath('tests'))
 from consts import *
-from brownie import chain, accounts, KeyManager, Vault, StakeManager, FLIP, chain
+from brownie import chain, accounts, KeyManager, Vault, StakeManager, FLIP, chain, network
 from deploy import deploy_initial_ChainFlip_contracts
 
+print(network.show_active())
 
 DEPLOYER = accounts[0]
 ALICE = accounts[1]
@@ -29,12 +30,12 @@ print("======================================================================")
 
 def all_stakeManager_events():
     print(f"\n💰 Alice stakes {MIN_STAKE} with nodeID {JUNK_INT}\n")
-    cf.stakeManager.stake(JUNK_INT, MIN_STAKE, {'from': ALICE})
+    cf.stakeManager.stake(JUNK_INT, MIN_STAKE, NON_ZERO_ADDR, {'from': ALICE})
 
     claim_amount = int(MIN_STAKE / 3)
     print(f"\n💰 Alice registers a claim for {claim_amount} with nodeID {JUNK_INT}\n")
     args = (JUNK_INT, claim_amount, ALICE, chain.time()+(2*CLAIM_DELAY))
-    callDataNoSig = cf.stakeManager.registerClaim.encode_input(NULL_SIG_DATA, *args)
+    callDataNoSig = cf.stakeManager.registerClaim.encode_input(agg_null_sig(), *args)
     cf.stakeManager.registerClaim(AGG_SIGNER_1.getSigData(callDataNoSig), *args)
 
     chain.sleep(CLAIM_DELAY)
@@ -44,10 +45,10 @@ def all_stakeManager_events():
 
     new_min_stake = int(MIN_STAKE / 3)
     print(f"\n💰 Denice sets the minimum stake to {new_min_stake}\n")
-    callDataNoSig = cf.stakeManager.setMinStake.encode_input(NULL_SIG_DATA, new_min_stake)
+    callDataNoSig = cf.stakeManager.setMinStake.encode_input(gov_null_sig(), new_min_stake)
     cf.stakeManager.setMinStake(GOV_SIGNER_1.getSigData(callDataNoSig), new_min_stake, {"from": DENICE})
 
     new_emission_per_block = int(EMISSION_PER_BLOCK / 3)
     print(f"\n💰 Denice sets the new emission per block to {new_emission_per_block}\n")
-    callDataNoSig = cf.stakeManager.setEmissionPerBlock.encode_input(NULL_SIG_DATA, new_emission_per_block)
+    callDataNoSig = cf.stakeManager.setEmissionPerBlock.encode_input(gov_null_sig(), new_emission_per_block)
     cf.stakeManager.setEmissionPerBlock(GOV_SIGNER_1.getSigData(callDataNoSig), new_emission_per_block, {"from": DENICE})
