c33e04dd21abc022836c32706f6cd3add22b7cca
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2021-09-17 02:04:20 +0200

    Only deploy the erc1820 registry on a non-live network

--- tests/deploy.py
+++ tests/deploy.py
@@ -12,7 +12,8 @@ def deploy_initial_ChainFlip_contracts(deployer, KeyManager, Vault, StakeManager
         pass
 
     cf = Context()
-    deploy_erc1820(deployer)
+    if network.show_active() == 'development' or network.show_active() == 'hardhat':
+        deploy_erc1820(deployer)
     cf.keyManager = deployer.deploy(KeyManager, AGG_SIGNER_1.getPubData(), GOV_SIGNER_1.getPubData())
     cf.vault = deployer.deploy(Vault, cf.keyManager)
     cf.stakeManager = deployer.deploy(StakeManager, cf.keyManager, MIN_STAKE, INIT_SUPPLY, NUM_GENESIS_VALIDATORS, GENESIS_STAKE)
