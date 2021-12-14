9bc979eacef92978f580960707a15fc2bb655fb7
Tom Nash <tom@chainflip.io>
2021-12-14 17:08:59 +0100

    Fix stupid case

--- contracts/StakeManager.sol
+++ contracts/StakeManager.sol
@@ -80,7 +80,7 @@ contract StakeManager is Shared, IStakeManager, IERC777Recipient {
         _defaultOperators.push(address(this));
         uint genesisValidatorFlip = numGenesisValidators * genesisStake;
         _totalStake = genesisValidatorFlip;
-        _FLIP = new FLIP("ChainFlip", "FLIP", _defaultOperators, address(this), flipTotalSupply);
+        _FLIP = new FLIP("Chainflip", "FLIP", _defaultOperators, address(this), flipTotalSupply);
         _FLIP.transfer(msg.sender, flipTotalSupply - genesisValidatorFlip);
         _ERC1820_REGISTRY.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
     }
