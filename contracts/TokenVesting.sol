756e001ef9d4ab6c8d375a501ab4e8b286a848c5
allimos3 <allimos3@gmail.com>
2022-03-07 09:01:31 +0100

    style: Compact flip approval
    Compact flip approval

--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -104,8 +104,7 @@ contract TokenVesting is ReentrancyGuard {
         require(msg.sender == beneficiary, "Vesting: not the beneficiary");
         require(canStake, "Vesting: cannot stake");
 
-        IERC20 _FLIP = stakeManager.getFLIP();
-        _FLIP.approve(address(stakeManager), amount);
+        stakeManager.getFLIP().approve(address(stakeManager), amount);
         stakeManager.stake(nodeID, amount, address(this));
     }
 
