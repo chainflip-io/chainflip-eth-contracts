6a57ea220eb84568a0d8063b916db289f2e53dfa
allimos3 <allimos3@gmail.com>
2022-03-14 11:07:52 +0100

    feat: Remove OZ counter dependency
    Remove OZ counter dependency

--- contracts/FLIP.sol
+++ contracts/FLIP.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "./interfaces/IFLIP.sol";
 import "./abstract/Shared.sol";
 
@@ -14,14 +13,12 @@ import "./abstract/Shared.sol";
  * @author   Quantaf1re (James Key)
  */
 contract FLIP is ERC20, ERC20Burnable, Ownable, Shared {
-    using Counters for Counters.Counter;
-
-    mapping(address => Counters.Counter) private _nonces;
+    mapping(address => uint256) private _nonces;
 
     string public constant VERSION = "1";
 
     // solhint-disable-next-line var-name-mixedcase
-    bytes32 private immutable DOMAIN_SEPARATOR;
+    bytes32 private immutable _DOMAIN_SEPARATOR;
 
     bytes32 private constant _PERMIT_TYPEHASH =
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
@@ -33,7 +30,7 @@ contract FLIP is ERC20, ERC20Burnable, Ownable, Shared {
         uint256 mintAmount,
         uint256 chainId_
     ) ERC20(name, symbol) Ownable() nzAddr(receiver) nzUint(mintAmount) {
-        DOMAIN_SEPARATOR = keccak256(
+        _DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                 keccak256(bytes(name)),
@@ -68,11 +65,11 @@ contract FLIP is ERC20, ERC20Burnable, Ownable, Shared {
         bytes32 digest = keccak256(
             abi.encodePacked(
                 "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _useNonce(owner), deadline))
+                _DOMAIN_SEPARATOR,
+                keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _nonces[owner], deadline))
             )
         );
-
+        _nonces[owner]++;
         require(owner != address(0), "FLIP: invalid permit zero address");
         require(owner == ecrecover(digest, v, r, s), "FLIP: invalid permit signature");
 
@@ -83,7 +80,7 @@ contract FLIP is ERC20, ERC20Burnable, Ownable, Shared {
      * @dev See {IERC20Permit-nonces}.
      */
     function nonces(address owner) public view returns (uint256) {
-        return _nonces[owner].current();
+        return _nonces[owner];
     }
 
     /**
@@ -91,15 +88,6 @@ contract FLIP is ERC20, ERC20Burnable, Ownable, Shared {
      */
     // solhint-disable-next-line func-name-mixedcase
     function domain_separator() external view returns (bytes32) {
-        return DOMAIN_SEPARATOR;
-    }
-
-    /**
-     * @dev "Consume a nonce": return the current value and increment.
-     */
-    function _useNonce(address owner) internal returns (uint256 current) {
-        Counters.Counter storage nonce = _nonces[owner];
-        current = nonce.current();
-        nonce.increment();
+        return _DOMAIN_SEPARATOR;
     }
 }
