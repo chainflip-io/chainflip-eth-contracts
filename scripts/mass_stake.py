f3e06b34082257c7b90ce20c652fea66d7638065
morelazers <tom@chainflip.io>
2022-04-05 15:27:53 +0200

    fix: set required confs to 1, this is horrible

--- scripts/mass_stake.py
+++ scripts/mass_stake.py
@@ -28,12 +28,12 @@ def main():
 		f.close()
 	staker = cf_accs[DEPLOYER_ACCOUNT_INDEX]
 	to_approve = flip.balanceOf(staker)
-	tx = flip.approve(stakeManager, to_approve, {"from": staker, "required_confs": 0})
+	tx = flip.approve(stakeManager, to_approve, {"from": staker, "required_confs": 1})
 	print(f'Approving {to_approve / 10**18} FLIP in tx {tx.txid}')
 	for i, node_id in enumerate(node_ids):
 		to_stake = stake + (i * 10**18)
 		node_id = node_id.strip()
-		tx = stakeManager.stake(node_id, to_stake, return_addr, {"from": staker, "required_confs": 0})
+		tx = stakeManager.stake(node_id, to_stake, return_addr, {"from": staker, "required_confs": 1})
 		print(f'Staking {to_stake / 10**18} FLIP to node {node_id} in tx {tx.txid}')
 
 def cleanHexStr(thing):
