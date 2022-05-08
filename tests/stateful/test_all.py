4f63e970575d1c51f6ec9b0f1e14bf910908ac56
Albert Llimos <53186777+albert-llimos@users.noreply.github.com>
2022-05-08 08:43:37 +0200

    fix: change settings style

--- tests/stateful/test_all.py
+++ tests/stateful/test_all.py
@@ -7,10 +7,10 @@ from utils import *
 from hypothesis import strategies as hypStrat
 from random import choice, choices
 
-settings = {"stateful_step_count": 100, "max_examples": 50, "deadline" = 500}
 
 
 # Stateful test for all functions in the Vault, KeyManager, and StakeManager
+@settings(stateful_step_count=100, max_examples=50, deadline=500)
 def test_all(
     BaseStateMachine,
     state_machine,
