e7224fd77423f4d4e1235e8381ec9228c4ede716
albert <allimos3@gmail.com>
2022-11-15 18:29:36 +0100

    feat: cleanup and more additions

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -374,85 +374,33 @@ contract Vault is IVault, AggKeyNonceConsumer, GovernanceCommunityGuarded {
     //                                                          //
     //////////////////////////////////////////////////////////////
 
-    // Input params:
-    // 1. bytes memory/calldata message
-    // 2. egress chain (string vs uint16 vs uint32). COuld be the chainID or just a number that in CF we map to a chain.
-    //      LZ uses their own chainId numerology
-    //      Connext also does that
-    //      Axelar has their specific strings
-    // 3. egress address (bytes32 or bytes or string to be more flexible? address is limiting to EVM).
-    //      BTC addresses are 26-35 characters long => Google says approximately 25 bytes but I don't know..
-    //      Also, converting that address into bytes might be extremely confusing. So I would fo for a string
-    //      for the egress address. The only downside is that it's not as gas efficient as bytes.
-    //      Example BTC address: bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh
-    //                => converted to bytes in Solidity:   0x20626331717879326b676479676a727371747a71326e30797266323439337038336b6b666a687830776c68
-    //                => that is more than 32 bytes:       0x626c756500000000000000000000000000000000000000000000000000000000
-    //                =>  representation before address is (https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses)
-    //                    converted into base  58 encoded: 0x00f54a5851e9372b87810a8e60cdd2e7cfd80b6e31
-    //      So it seems like there is a way to get BTC addresses in 25 bytes
-    //      So we could potentially only use bytes (not bytes32). Both could work. For BTC, having the address in bytes is very confusing,
-    //      although for other chains. Also, looks like Solana addresses are similar to BTC (2zL8VtonfKdGxj15qwF1vi8xHPcrC89WWNDccq2tFMP9)
-    //      potentially even longer (32-55 characters). So it is not only BTC that will look weird on-chain if using bytes.
-    //      SUI and APTOS are more friendly (32 hex characters).
-    //
-    // 4. egress token
-    //      - address => This will limit the non-EVM chains.
-    //      - string?
-    //      - This could also be embedded in a egress address string (ETH:ETH) and the CF engine will translate
-    //        it into the correct address. That is probably the most gas-efficient way to do it.
-    //      - This could even be a bool (_isNative). This works if we only support two assets per chain. In ETH
-    //        for example we support 3 (USDC/ETH/FLIP) so I don't think that would work. It also limits us
-    //        in the future. Like before, the CF engine would need to convert it into the correct address.
-    //      - uint => we could have a mapping of uint => token for each chain. Would be a lot more gas efficient and
-    //        easier to integrate with other protocols.
-    // 5- ingress token => This needs to be an address/ERC20 (isNative would be a mess and potentially not work with 3 assets)
-    // 6. amount: Only needed for non-native tokens. For native tokens, we can use msg.value.
-
-    // Should we split this into two functions, one for native and one for non-native?
-    // Should we have the non-cross message in a separate function? Or just mark it with empty message?
-
-    // TLDR: BTC and Solana (and maybe others) have addresses expressed in characters (non-hex). They can maybe be expressed in
-    // less than 32 bytes but there would need to be some non-intuitive conversion. A conversion from the address in string to
-    // bytes would be also confusing to read on-chain and the final result is >32 bytes.
-    // For addresses in 32 bytes it is more expensive to use string and also to use bytes. For more than 32 bytes, we can use bytes
-    // or string. The gas usage of both is exactly the same (assuming we are converting stringToBytes as Solidity would do).
-    // Therefore, I would go for a string which is more readable on-chain and very flexible - allow for all chains.
-
-    // So the remaining questions are:
-    // 1. Can/should we just represent the egress address as a uint or string? String will definitely take more gas but we can pack it with other egress,
-    // althought that will still me more gas costly. Depends on future flexibility (although uint should be enough) and mainly on clarity and ease for CFE.
-    // 2. EgressToken and EgressAddress seem to need a string. Then the CFE will translate it to addresses in the egress chain. Should we pack them?
-    // 3. If we pack the two previous values, should we also pack it with the first one? Or have two separate strings, one with the egress chain
-    // and egress token, and the other one with the egress address. The only upside there would be clarity.
-    // If we pack them all together we could separate them with ":" or something, so we can split them in the CFE.
-    // Packing all them would especially be good since the string overhead will be only once. However, egress chain would still be cheaper with a uint,
-    // and I am not sure how we feel about the clarity of "BTC:BTC:bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh", espeically for hex addresses with
-    // the "0x" at the beginning "ETH:USDC:0x362fA9D0bCa5D19f743Db50738345ce2b40eC99f".
-
-
-    // EgressParams being egressChain:egressToken
-    // We could have egressParams be two uints (same gas) and map each uint to a chain and a token.
+    // We could have egressParams be two uints (same gas) instead of a string, and map each uint to a chain and a token (off-chain).
     // In the function call the parameters are not packed, so it doesnt matter if it's two uint256 or two uint32.
     // Actually, string concatenation after solidity 8.12 is easy, just string.concat(). But the code will need to concat with ":". It costs
     // gas but it's OK to ask that since it will normally come externally. If it doesn't they can just concatenate it.
     // Checking non-empty string (egressParams/egressAddress) is a bit of a waste of gas, so we don't do it.
     // Also, because a non-empty string doesn't mean it's a valid egressAddress anyway. That checking should
     // be done by the CFE.
+
     // TODO: if we refund we need to add a refund address. For example, if LiFi is in the middle, we want to refund the user, not LiFi.
     // This is the case if we do a retrospective refund.
     // TODO: Think about error handling (e.g. addGas? How do we then refer to a specific transaction? transferID? Counter? Nonce?
     //       Or we just give the transaction to the user to send?
     // TODO: If we want to allow gas topups then a txHash parameter should be use as an input to match with the transaction. This way
     //       there wouldn't be a need to add a transferID at the smart contract level. But then the SC needs to keep track of that.
-    // TODO: Do we want a pure CCM call without token swapping?. I think so.
+    // TODO: Do we want a pure CCM call without token swapping?. I think so for the future. Maybe enabled/disabled by a flag.
     // TODO: Do we prefer uint for egressChain and egress token or a single string? Gaswise the uints are just a bit cheaper (not too relevant)
     //       However, that would be great for the egress part, since we only pass egress chain there.
+    // TODO: I don't think we need separate functions for swaps with non-CCM since those can be done via ingressAddress instead.
+    //       The gas difference seems to be around 2k (because we also need to emit that). If it's coming from a protocol and not a user (e.g.LiFi)
+    //       then it's true that those 2k is less relevant, but it might still be worth it for us since we have bytecodesize left.
+    //       Depends on how much we want to optimize for non-ccm swaps done through the Vault.
 
     /**
      * @notice  Swaps ERC20 Token for a token in another chain. Function call needs to specify the ingress and egress parameters.
      *          It also has a cross-chain message capability. An empty message signifies that only a token swap is to be performed.
      * @param egressParams  String containing egressChain and egressToken. Most likely egressChain:egressToken. In
-     *                      the event of this not coming from calldata, it can easily be concatenated after 
+     *                      the event of this not coming from calldata, it can easily be concatenated after
      *                      after solidity 8.12 with string.concat().
      *                      We could also make this two parameters two uints which is slightly cheaper gas-wise.
      * @param egressAddress String containing the egress address. In case of the sending contract having to craft
@@ -482,18 +430,7 @@ contract Vault is IVault, AggKeyNonceConsumer, GovernanceCommunityGuarded {
         emit SwapETH(egressParams, egressAddress, message, msg.value, msg.sender);
     }
 
-    function ccm(
-        string memory egressChain,
-        string memory egressAddress,
-        bytes calldata message
-    ) external payable onlyNotSuspended swapsEnabled {
-        // The check for existing parameters shall be done in the CFE
-        emit CCM(egressChain, egressAddress, message, msg.sender);
-    }
-
-    // Source token is not really needed right? if so, I'm not sure it should be part of that string, since it's probably not what the user would want to check.
-    // Separating ingressChain and ingressAddress because if they want to be checked separately it's extremely painful to split them in solidity.
-    function crossChainMessage(
+    function swapWithCCM(
         SigData calldata sigData,
         TransferParams calldata transferParams,
         string calldata ingressChain,
@@ -502,6 +439,7 @@ contract Vault is IVault, AggKeyNonceConsumer, GovernanceCommunityGuarded {
     )
         external
         onlyNotSuspended
+        swapsEnabled
         nzAddr(address(transferParams.token))
         nzAddr(transferParams.recipient)
         nzUint(transferParams.amount)
@@ -509,7 +447,7 @@ contract Vault is IVault, AggKeyNonceConsumer, GovernanceCommunityGuarded {
             sigData,
             keccak256(
                 abi.encodeWithSelector(
-                    this.crossChainMessage.selector,
+                    this.swapWithCCM.selector,
                     SigData(sigData.keyManAddr, sigData.chainID, 0, 0, sigData.nonce, address(0)),
                     transferParams,
                     ingressChain,
@@ -524,19 +462,31 @@ contract Vault is IVault, AggKeyNonceConsumer, GovernanceCommunityGuarded {
         uint256 amount = transferParams.amount;
         address token = address(transferParams.token);
         address payable recipient = transferParams.recipient;
-         _transfer(token, recipient, amount);
+        _transfer(token, recipient, amount);
         ICFReceiver(recipient).cfRecieve(ingressChain, ingressAddress, message, token, amount);
+    }
+
+    //////////////////////////////////////////////////////////////
+    //                                                          //
+    //                        CCM                               //
+    //                                                          //
+    //////////////////////////////////////////////////////////////
+
+    //TODO: Decide if we need/want this. Maybe we want to have it but disabled by default.
+    function ingressCCM(
+        string memory egressChain,
+        string memory egressAddress,
+        bytes calldata message
+    ) external payable onlyNotSuspended swapsEnabled {
+        // The check for existing parameters shall be done in the CFE
+        emit CCM(egressChain, egressAddress, message, msg.sender);
     }
 
     //TODO: Decide if we need this or we just take it as subset of transferWithMessage (with amount = 0).
     // This would be to have a specialized CCM messaging functionality. I think it would be good to have
     // so we don't have to pass a token an amount which doesn't make sense and wastes gas.
     // Even if we don't implement that straight away in Rust, it's good to have already in the SC.
-
-    // Ingress params = SourceChain:sourceAddress
-    // Do we want to have this? Or do we need this? It would be for cross-chain messaging without token transfer.
-    // I think that if we are not reaching the MAX bytecodesize we should have this for future-proofing.
-    function messageWithoutTransfer(
+    function egressCCM(
         SigData calldata sigData,
         address recipient,
         string calldata ingressChain,
@@ -550,7 +500,7 @@ contract Vault is IVault, AggKeyNonceConsumer, GovernanceCommunityGuarded {
             sigData,
             keccak256(
                 abi.encodeWithSelector(
-                    this.messageWithoutTransfer.selector,
+                    this.egressCCM.selector,
                     SigData(sigData.keyManAddr, sigData.chainID, 0, 0, sigData.nonce, address(0)),
                     recipient,
                     ingressChain,
