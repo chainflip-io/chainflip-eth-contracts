66333b2aa147a7d88b6b6d9d2b8d8be6f49895bb
albert <allimos3@gmail.com>
2022-12-13 14:57:19 +0100

    feat: succesfully connected to ibiza

--- scripts/debuggingTool.py
+++ scripts/debuggingTool.py
@@ -6,30 +6,49 @@ from consts import *
 
 from brownie import accounts, StakeManager, FLIP, chain, web3
 
-# FLIP_ADDRESS = environ["FLIP_ADDRESS"]
-# STAKE_MANAGER_ADDRESS = environ["STAKE_MANAGER_ADDRESS"]
-# AUTONOMY_SEED = environ["SEED"]
+# TODO: To add other contracts
+FLIP_ADDRESS = environ["FLIP_ADDRESS"]
+STAKE_MANAGER_ADDRESS = environ["STAKE_MANAGER_ADDRESS"]
 
-# DEPLOYER_ACCOUNT_INDEX = int(environ.get("DEPLOYER_ACCOUNT_INDEX") or 0)
 
-# cf_accs = accounts.from_mnemonic(AUTONOMY_SEED, count=10)
+AUTONOMY_SEED = environ["SEED"]
+DEPLOYER_ACCOUNT_INDEX = int(environ.get("DEPLOYER_ACCOUNT_INDEX") or 0)
 
-# node_ids = []
+cf_accs = accounts.from_mnemonic(AUTONOMY_SEED, count=10)
+userAddress = cf_accs[DEPLOYER_ACCOUNT_INDEX]
+
+flip = FLIP.at(FLIP_ADDRESS)
 
 # stake = 10**3 * E_18
-return_addr = "0xffffffffffffffffffffffffffffffffffffffff"
+#return_addr = "0xffffffffffffffffffffffffffffffffffffffff"
 
 # Define a dictionary of available commands and their corresponding functions
 commands = {
     "help": (lambda: help(), "Prints help"),
     "hello": (lambda: print("Hello, world!"), "Prints 'Hello, world!'"),
-    "balance": (lambda a: balance(a), "Get the balance of an account"),
+    "balance": (lambda a: balance(a), "Get the balance of an account",),
+    "balanceFlip": (lambda a: balanceFlip(a), "Get the balance of an account",),
+    "contracts": (lambda: print(contractAddresses), "Prints addresses"),
+    "user": (lambda: print(userAddress), "Prints current user address"),
+    "walletAddresses": (lambda : print(walletAddresses), "Show wallet addresses"),
+    "changeAddress": (lambda i: changeAddress(i), "Change selected address"),
     #"add": (lambda x, y: print(x + y), "Adds two numbers together"),
     "exit": (lambda: exit(), "Exits the program")
 }
 
+contractAddresses = {
+    "flip": FLIP_ADDRESS,
+    "stakeManager": STAKE_MANAGER_ADDRESS,
+}
+
+walletAddresses = {}
+seedNumber = 1
+for cf_acc in cf_accs:
+    walletAddresses[str(seedNumber)] = cf_acc
+    seedNumber += 1
+
 def main():
-    # flip = FLIP.at(f"0x{cleanHexStr(FLIP_ADDRESS)}")
+    flip = FLIP.at(f"0x{cleanHexStr(FLIP_ADDRESS)}")
     # stakeManager = StakeManager.at(f"0x{cleanHexStr(STAKE_MANAGER_ADDRESS)}")
     # sender = cf_accs[DEPLOYER_ACCOUNT_INDEX]
 
@@ -61,12 +80,32 @@ def main():
 
 def help():
     # Print the available commands and their descriptions
-    for name,(_, description) in commands.items():
-        print("   -" , f"{name}: {description}")
+    print("\nUsage:  command <arg0> <arg1> ... <argN>\n")
+    print("Available commands:")
+    for name,(func, description) in commands.items():
+        print("{0:17} {1}".format("  "+name, description))
+        # TODO: Add argument list and description to help.
+        argcount = func.__code__.co_argcount
+        #print(" argcount: ", argcount)
+
 
 def balance(a):
     print("ETH Balance of", a, ":", web3.eth.get_balance(a))
 
+def balanceFlip(a):
+    global flip
+    print("FLIP Balance of", a, ":", flip.balanceOf(a))
+
+def changeAddress(accountIndex):
+    accountIndex = int(accountIndex)
+    if accountIndex > 9:
+        print("Account index out of range")
+        return
+
+    global userAddress 
+    userAddress = cf_accs[accountIndex]
+
+    print("New user address: ", userAddress)
 
 
 # def stake():
