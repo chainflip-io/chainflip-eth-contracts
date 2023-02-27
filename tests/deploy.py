fdbf2970707aa333bbe0eb0502ee29581d4fdf02
albert <allimos3@gmail.com>
2023-02-27 12:17:33 +0200

    chore: lint

--- tests/deploy.py
+++ tests/deploy.py
@@ -66,7 +66,7 @@ def deploy_initial_Chainflip_contracts(
         cf.keyManager,
         MIN_STAKE,
     )
-    # Minting genesis validator FLIP to the Stake Manager. The rest of genesis FLIP will 
+    # Minting genesis validator FLIP to the Stake Manager. The rest of genesis FLIP will
     # be minted to the governance address for safekeeping.
     cf.flip = deployer.deploy(
         FLIP,
