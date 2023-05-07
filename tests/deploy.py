c48e5ccb281b3febec7c7d6d5c6ec0b91364b9a4
albert <allimos3@gmail.com>
2023-05-07 14:29:16 +0200

    fix: lint

--- tests/deploy.py
+++ tests/deploy.py
@@ -55,7 +55,6 @@ def deploy_Chainflip_contracts(
         f"Deploying with NUM_GENESIS_VALIDATORS: {cf.numGenesisValidators}, GENESIS_STAKE: {cf.genesisStake}"
     )
 
-
     # Deploying in live networks sometimes throws an error when getting the address of the deployed contract.
     # I suspect that the RPC nodes might not have processed the transaction. Increasing the required confirmations
     # to more than one is a problem in local networks with hardhat's automining enabled, as it will brick
@@ -64,7 +63,7 @@ def deploy_Chainflip_contracts(
     required_confs = 1 if chain.id == 31337 else 4
 
     # Deploy contracts via cf.deployerContract. Minting genesis validator FLIP to the State Chain Gateway.
-    # The rest of genesis FLIP will be minted to the governance address for safekeeping.     
+    # The rest of genesis FLIP will be minted to the governance address for safekeeping.
     cf.deployerContract = DeployerContract.deploy(
         aggKey,
         cf.gov,
