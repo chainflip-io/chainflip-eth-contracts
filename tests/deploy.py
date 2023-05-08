31a6adda9fcd9dd1182b94a421aed44c3e549da2
albert <allimos3@gmail.com>
2023-05-08 13:33:47 +0200

    chore: only live networks

--- tests/deploy.py
+++ tests/deploy.py
@@ -58,9 +58,9 @@ def deploy_Chainflip_contracts(
     # Deploying in live networks sometimes throws an error when getting the address of the deployed contract.
     # I suspect that the RPC nodes might not have processed the transaction. Increasing the required confirmations
     # to more than one is a problem in local networks with hardhat's automining enabled, as it will brick
-    # the script. Therefore, we increase the required_confs for live networks only.
-    # Local local nets, including forks, are identified by chain id 31337.
-    required_confs = 1 if chain.id == 31337 else 4
+    # the script. Therefore, we increase the required_confs for live networks only. No need to do it for testing
+    # nor localnets/devnets - that is with hardhat (including forks), with id 31337, and geth image, with id 10997.
+    required_confs = 1 if (chain.id == 31337 or chain.id == 10997) else 4
 
     # Deploy contracts via cf.deployerContract. Minting genesis validator FLIP to the State Chain Gateway.
     # The rest of genesis FLIP will be minted to the governance address for safekeeping.
