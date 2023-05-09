42267dbe0a5d7d57e0d2472b2104d97386b4a245
albert <allimos3@gmail.com>
2023-05-09 19:05:02 +0200

    chore: fix bug

---
+++ tests/token_vesting/scgatewayReference/test_scgatewayReference.py
@@ -0,0 +1,4 @@
+# TODO: Probably add some other test to the TokenVesting to ensure that if we
+# change the reference while their tokens are staked they can still be withdrawn.
+# The only "issue" will be when staking again, but that's fine.
+# TODO: Write tests for the scgateway reference contract.
