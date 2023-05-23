c198ffef34cd403b2789614a59dd4a65eb401082
albert <allimos3@gmail.com>
2023-05-23 16:31:07 +0200

    chore: try with 22.04

--- contracts/echidna/tests/NewTestEchidna-flat.sol
+++ contracts/echidna/tests/NewTestEchidna-flat.sol
@@ -1,6 +1,17 @@
 pragma solidity 0.8.20;
 IHevm constant hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
+// function bytesToString(bytes memory data) pure returns(string memory) {
+//     uint256 len = 2*data.length;
+//     bytes memory out = new bytes(len);
+//     bytes memory hexdigits = "0123456789abcdef";
+
+//     for (uint i = 0; i < data.length; i++) {
+//         out[i*2] = hexdigits[uint(uint8(data[i]) >> 4)];
+//         out[i*2+1] = hexdigits[uint(uint8(data[i]) & 0x0f)];
+//     }
+//     return string(out);
+// }
 function bytesToString(bytes memory data) pure returns (string memory) {
     uint256 len = 2 * data.length;
     bytes memory out = new bytes(len);
@@ -1126,32 +1137,6 @@ contract KeyManager is SchnorrSECP256K1, Shared, IKeyManager {
         _consumeKeyNonce(sigData, msgHash);
         _;
     }
-}
-
-interface IHevm {
-    // Set block.timestamp to newTimestamp
-    function warp(uint256 newTimestamp) external;
-
-    // Set block.number to newNumber
-    function roll(uint256 newNumber) external;
-
-    // Loads a storage slot from an address
-    function load(address where, bytes32 slot) external returns (bytes32);
-
-    // Stores a value to an address' storage slot
-    function store(address where, bytes32 slot, bytes32 value) external;
-
-    // Signs data (privateKey, digest) => (r, v, s)
-    function sign(uint256 privateKey, bytes32 digest) external returns (uint8 r, bytes32 v, bytes32 s);
-
-    // Gets address for a given private key
-    function addr(uint256 privateKey) external returns (address addr);
-
-    // Performs a foreign function call via terminal
-    function ffi(string[] calldata inputs) external returns (bytes memory result);
-
-    // Performs the next smart contract call with specified `msg.sender`
-    function prank(address newSender) external;
 }
 
 interface IGovernanceCommunityGuarded is IShared {
@@ -3391,6 +3376,32 @@ contract DeployerContract is IShared {
         stateChainGateway = _stateChainGateway;
         flip = _flip;
     }
+}
+
+interface IHevm {
+    // Set block.timestamp to newTimestamp
+    function warp(uint256 newTimestamp) external;
+
+    // Set block.number to newNumber
+    function roll(uint256 newNumber) external;
+
+    // Loads a storage slot from an address
+    function load(address where, bytes32 slot) external returns (bytes32);
+
+    // Stores a value to an address' storage slot
+    function store(address where, bytes32 slot, bytes32 value) external;
+
+    // Signs data (privateKey, digest) => (r, v, s)
+    function sign(uint256 privateKey, bytes32 digest) external returns (uint8 r, bytes32 v, bytes32 s);
+
+    // Gets address for a given private key
+    function addr(uint256 privateKey) external returns (address addr);
+
+    // Performs a foreign function call via terminal
+    function ffi(string[] calldata inputs) external returns (bytes memory result);
+
+    // Performs the next smart contract call with specified `msg.sender`
+    function prank(address newSender) external;
 }
 
 abstract contract CFReceiver is ICFReceiver {
