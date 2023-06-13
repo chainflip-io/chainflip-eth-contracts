56f34cbf68db91004ec1e02e1c75252b116a2047
albert <allimos3@gmail.com>
2023-06-13 17:46:08 +0200

    chore: first full implementation BalanceChecker

--- contracts/helpers/BalanceChecker.sol
+++ contracts/helpers/BalanceChecker.sol
@@ -7,31 +7,72 @@ pragma solidity ^0.8.0;
  * @notice   Checks the Native token balances of multiple addresses. This can be used to check
  *           the balances of multiple addresses in a single call instead of issuing a separate
  *           call for each address, which is very innefficient.
- * @dev      This is a view function so it doesn't spend gas. However, some RPC providers put a
- *           cap on the amount of gas that the transaction *would* consume to prevent spamming.
- *           I believe the geth client also has a similar feature. As a rough refernece, a try
- *           using a free INFURA endpoint succesfully executes around 75k addresses but fails
- *           when requesting 100k. It's advised to split up such big lists.
+ * @dev      This contract only contains view functions that doesn't spend gas. However, some RPC
+ *           providers put a cap on the amount of gas that the transaction *would* consume to
+ *           prevent spamming. As a rough refernece, a try using a free INFURA endpoint succesfully
+ *           executes around 75k addresses but fails when requesting 100k. It's advised to split
+ *           up such big lists.
  */
 contract BalanceChecker {
+    struct BalanceAndDeployedStatus {
+        uint256 balance;
+        bool deployed;
+    }
+
     /**
      * @notice  Returns an array of native token balances for an array of addresses.
-     * @dev     Even though this function is a view function and should not spend gas when called
-     *          directly, there can be some limitation on the amount of computation that it can do.
-     *          Some node providers put a cap on the amount of computation/gas that can be used.
-     * @param addresses  Array of addresses to get the balance of.
+     * @param addresses  Array of addresses to check.
      */
-    function balancesNative(address[] calldata addresses) external view returns (uint[] memory) {
+    function getNativeBalances(address[] calldata addresses) external view returns (uint[] memory) {
         uint256 length = addresses.length;
 
-        uint[] memory addrBalances = new uint[](length);
+        uint[] memory balances = new uint[](length);
 
         for (uint i = 0; i < length; ) {
-            addrBalances[i] = addresses[i].balance;
+            balances[i] = addresses[i].balance;
             unchecked {
                 ++i;
             }
         }
-        return addrBalances;
+        return balances;
+    }
+
+    /**
+     * @notice  Returns an array of booleans signaling whether there is bytecode deployed for an array of addresses.
+     * @param addresses  Array of addresses to check.
+     */
+    function getDeployedStatus(address[] calldata addresses) external view returns (bool[] memory) {
+        uint256 length = addresses.length;
+
+        bool[] memory deployedStatus = new bool[](length);
+
+        for (uint i = 0; i < length; ) {
+            deployedStatus[i] = addresses[i].code.length > 0;
+            unchecked {
+                ++i;
+            }
+        }
+        return deployedStatus;
+    }
+
+    /**
+     * @notice  Returns an array of structs with both the native token balance and whether the addresses have bytecode
+     *          deployed for for an array of addresses.
+     * @param addresses  Array of addresses to check.
+     */
+    function getNativeBalancesAndDeployedStatus(
+        address[] calldata addresses
+    ) external view returns (BalanceAndDeployedStatus[] memory) {
+        uint256 length = addresses.length;
+
+        BalanceAndDeployedStatus[] memory balanceAndDeployedStatus = new BalanceAndDeployedStatus[](length * 2);
+
+        for (uint i = 0; i < length; ) {
+            balanceAndDeployedStatus[i] = BalanceAndDeployedStatus(addresses[i].balance, addresses[i].code.length > 0);
+            unchecked {
+                ++i;
+            }
+        }
+        return balanceAndDeployedStatus;
     }
 }
--- tests/unit/helpers/test_balanceChecker.py
+++ tests/unit/helpers/test_balanceChecker.py
@@ -11,19 +11,22 @@ def test_balanceChecker(cf, st_addresses):
     addresses_balances = [
         web3.eth.get_balance(str(address)) for address in st_addresses
     ]
-    assert cf.balanceChecker.balancesNative(st_addresses) == addresses_balances
+    assert cf.balanceChecker.getNativeBalances(st_addresses) == addresses_balances
 
 
 def test_balanceChecker_gas(cf):
     # More than this returns a "Transaction ran out of gas" in testing. However, in a real network
     # we should be able to query more addresses.
-    number_of_addresses = 28000
+    number_of_addresses = 27000
     list_of_addresses = [
         "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
     ] * number_of_addresses
-    balances = cf.balanceChecker.balancesNative(list_of_addresses)
+    balances = cf.balanceChecker.getNativeBalances(list_of_addresses)
     assert len(balances) == number_of_addresses
     assert all(
         balance == web3.eth.get_balance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
         for balance in balances
     )
+
+
+## TODO: Add test for getDeployedStatus and getNativeBalancesAndDeployedStatus
