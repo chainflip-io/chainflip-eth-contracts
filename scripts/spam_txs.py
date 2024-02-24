b6b6bd790ca7c40d91d2cef6d67c42d3a21fe821
albert <allimos3@gmail.com>
2024-02-24 07:52:02 +0100

    chore: add spam before snapshot

---
+++ scripts/spam_txs.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath("tests"))
+from shared_tests import *
+from utils import prompt_user_continue_or_break
+from consts import *
+from brownie import accounts, network
+
+AUTONOMY_SEED = os.environ["SEED"]
+cf_accs = accounts.from_mnemonic(AUTONOMY_SEED, count=10)
+DEPLOYER_ACCOUNT_INDEX = int(os.environ.get("DEPLOYER_ACCOUNT_INDEX") or 0)
+
+DEPLOYER = cf_accs[DEPLOYER_ACCOUNT_INDEX]
+print(f"DEPLOYER = {DEPLOYER}")
+network.priority_fee("1 gwei")
+
+# NUM_SPAM_TXS = int(os.environ["NUM_SPAM_TXS"])
+NUM_SPAM_TXS = 100
+
+
+def main():
+    for i in range(NUM_SPAM_TXS):
+        DEPLOYER.transfer(DEPLOYER, "1 ether")
