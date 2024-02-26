dc317b3281fa6a66af8d10e26e534d723e246347
albert <allimos3@gmail.com>
2024-02-26 07:58:39 +0100

    chore: cleanup

--- scripts/spam_txs.py
+++ scripts/spam_txs.py
@@ -1,9 +1,4 @@
-import sys
 import os
-
-sys.path.append(os.path.abspath("tests"))
-from shared_tests import *
-from consts import *
 from brownie import accounts, network
 
 AUTONOMY_SEED = os.environ["SEED"]
@@ -15,10 +10,9 @@ DEPLOYER = cf_accs[DEPLOYER_ACCOUNT_INDEX]
 print(f"DEPLOYER = {DEPLOYER}")
 network.priority_fee("1 gwei")
 
-# NUM_SPAM_TXS = int(os.environ["NUM_SPAM_TXS"])
 NUM_SPAM_TXS = 100
 
 
 def main():
-    for i in range(NUM_SPAM_TXS):
+    for _ in range(NUM_SPAM_TXS):
         DEPLOYER.transfer(DEPLOYER, "1 ether")
