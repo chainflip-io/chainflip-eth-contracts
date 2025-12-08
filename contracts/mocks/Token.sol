5e833aa552c41b019a1a4630132f0b5d7daf4398
Albert Llimos <allimos3@gmail.com>
2025-12-08 12:19:02 +0100

    chore: lint

--- contracts/mocks/Token.sol
+++ contracts/mocks/Token.sol
@@ -7,14 +7,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @dev      Creates a standard ERC20 just for the purposes of testing
  */
 contract Token is ERC20 {
-    uint8 immutable DECIMALS;
+    uint8 private immutable _decimals;
 
     constructor(string memory name, string memory symbol, uint256 mintAmount, uint8 decimals) ERC20(name, symbol) {
         _mint(msg.sender, mintAmount);
-        DECIMALS = decimals;
+        _decimals = decimals;
     }
 
     function decimals() public view virtual override returns (uint8) {
-        return DECIMALS;
+        return _decimals;
     }
 }
