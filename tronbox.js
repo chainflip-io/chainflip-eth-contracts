f2b49cd9b6b7d447f2e2800b7a0d114edf25d334
Albert Llimos <allimos3@gmail.com>
2026-02-27 12:50:39 +0100

    chore: add energy_limit to tronbox

--- tronbox.js
+++ tronbox.js
@@ -1,3 +1,6 @@
+// TODO: We probably want to set the  userFeePercentage and originEnergyLimit on e per contract
+// deployment instead to only cover Vault calls.
+// e.g. deployer.deploy(MyContract, { feeLimit: 1000000000, callValue: 0,  originEnergyLimit: 10000000});
 module.exports = {
   networks: {
     development: {
@@ -9,6 +12,7 @@ module.exports = {
       feeLimit: 1000 * 1e6, // The TRX consumption limit for the deployment and trigger, unit is SUN
       fullHost: "http://127.0.0.1:9090",
       network_id: "*",
+      originEnergyLimit: 10_000_000, // Default origin energy limit for contract deployment
     },
     nile: {
       // Obtain test coin at https://nileex.io/join/getJoinPage
@@ -20,6 +24,7 @@ module.exports = {
       feeLimit: 1000 * 1e6,
       fullHost: "https://nile.trongrid.io",
       network_id: "3",
+      originEnergyLimit: 10_000_000, // Default origin energy limit for contract deployment
     },
     mainnet: {
       privateKey: process.env.PRIVATE_KEY,
@@ -27,6 +32,7 @@ module.exports = {
       feeLimit: 500 * 1e6, // 500 TRX
       fullHost: "https://api.trongrid.io",
       network_id: "1",
+      originEnergyLimit: 10_000_000, // Default origin energy limit for contract deployment
     },
   },
   compilers: {
