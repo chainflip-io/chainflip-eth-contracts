8eb22a8452e2804e2acf5c0e63c4c5ea67537211
Albert Llimos <allimos3@gmail.com>
2026-07-09 14:08:28 +0200

    chore: update tests

--- tests/unit/keyManager/test_setGovKeyWithAggKey.py
+++ tests/unit/keyManager/test_setGovKeyWithAggKey.py
@@ -7,10 +7,3 @@ from shared_tests import *
 
 def test_setGovKeyWithAggKey(cf):
     setGovKeyWithAggKey_test(cf)
-
-
-def test_setGovKeyWithAggKey_rev(cf):
-    with reverts(REV_MSG_NZ_ADDR):
-        signed_call_cf(
-            cf, cf.keyManager.setGovKeyWithAggKey, ZERO_ADDR, sender=cf.ALICE
-        )
